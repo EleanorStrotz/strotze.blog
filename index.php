58ff8ae99fdfbb8561352f61fa327d7a6c41c2f7
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-03 11:58:49 -0800

    made a header and footer file for index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 <?php
-/* Creates/links Database to index.php*/
- require_once(__DIR__ . "/controller/creative-db.php");
+	require_once(__DIR__ . "/view/header.php");
+	/* Creates/links Database to index.php*/
+ 	require_once(__DIR__ . "/controller/creative-db.php");
+ 	require_once(__DIR__ . "/view/footer.php");
 ?>
-</body>
-</html>
+
