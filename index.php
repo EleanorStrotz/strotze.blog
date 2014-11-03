cc9aaf9d8dc94c5f537b801ea8f17f6d2fbe59fe
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-03 12:00:38 -0800

    added comments to code

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+    // require__once links index.php with header.php
 	require_once(__DIR__ . "/view/header.php");
 	/* Creates/links Database to index.php*/
  	require_once(__DIR__ . "/controller/creative-db.php");
+ 	// require_once links index.php with footer.php
  	require_once(__DIR__ . "/view/footer.php");
 ?>
 
