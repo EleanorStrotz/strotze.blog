8b0e7a18ebe0cb705bc7b15aa413cc5f9be0f3cd
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-03 12:14:41 -0800

    added navigation.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     // require__once links index.php with header.php
 	require_once(__DIR__ . "/view/header.php");
+	/* Creates/links Database to navigation.php*/
+	require_once(__DIR__ . "/view/navigation.php");
 	/* Creates/links Database to index.php*/
  	require_once(__DIR__ . "/controller/creative-db.php");
  	// require_once links index.php with footer.php
---
+++ view/navigation.php
@@ -0,0 +1,7 @@
+<!-- links you to post.php -->
+<nav>
+	<ul>
+		<li><a href="../post.php">Blog Post Form</a></li>
+	</ul>
+
+</nav>
