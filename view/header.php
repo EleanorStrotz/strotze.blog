2d3c6cacd86a89f766917755ada9ca29369fc986
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-04 10:55:06 -0800

    created a css stylesheet

--- view/header.php
+++ view/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title></title>
 </head>
 <body>
