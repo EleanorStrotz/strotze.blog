c667444e71b91d0e4f13bdbe2b24c9d579753e67
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-06 10:58:51 -0800

    submit button implimented

--- view/form.php
+++ view/form.php
@@ -1,13 +1,18 @@
 <h1>Create Blog Post</h1>
 
 <form>
+<!-- div puts in the title and the label for the title -->
 	<div>
 		<label for="title">Title: </label>
 		<input type="text" name="title" />
 	</div>
-
+<!-- div is a label for post -->
 	<div>
 		<label for="post">Post: </label>
 		<textarea name="post"></textarea>
+	</div>
+	<!-- div is to create the submit button -->
+	<div>
+		<button type="submit">Submit</button>
 	</div>
 </form>
