510f528d36c9d8c2bd5ea80175a0af0c457b25a2
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-07 09:58:44 -0800

    created create-post.php

---
+++ controller/create-post.php
@@ -0,0 +1,9 @@
+<?php
+//variable filter the input, gets the posts from the title and filters its to make sure its a string
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+	// variable filter the input, gets the posts from the post and filters it to make sure its a string
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+//echos title and post
+	echo "<p>Title: $title</p>";
+	echo "<p>Post: $post</p>";
+?>
