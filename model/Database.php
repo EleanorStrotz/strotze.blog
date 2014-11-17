292d580d6af2b66f05b80981a14b376e95665801
EleanorStrotz <EleanorStrotz@gmail.com>
2014-11-17 11:52:37 -0800

    video 27 - creating a constructor for our database class

--- model/Database.php
+++ model/Database.php
@@ -9,6 +9,16 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+
+    // The constructor is called on an object after it has been created, and it is a good place to put initialisation code.
+    // the variables inside of the braces are global variables
+	public function __construct($host, $username, $password, $database){
+		// accesses global variables by using the key word this then the arrow which will give us access to the global variables.
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
 }
 
 ?>
