e0bff9d5463bcda301a36b55d3a5f3df2eb311bc
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-02 11:08:41 -0800

    video 37- creating registration form

---
+++ view/register-form.php
@@ -0,0 +1,22 @@
+<!-- Form created so user can register so they can be able to post, posts -->
+<form>
+<!-- label for user to register their email -->
+	<div>
+		<label for="email">Email: </label>
+		<input type="text" name="email" />
+	</div>
+<!-- label for user to register there user name-->
+	<div>
+	<label for="username">Username: </label>
+	<input type="text" name="username" />
+	</div>
+<!-- label for user to input their password; password appears as dots not text -->
+	<div>
+	<label for="password">Password: </label>
+	<input type="text" name="password" />	
+	</div>
+<!-- button to submit the info -->
+	<div>
+		<button type="submit" >Submit</button>
+	</div>
+</form>
