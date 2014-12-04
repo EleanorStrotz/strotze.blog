336e6246243d372e92b82c35a6954c5121882153
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-04 11:08:02 -0800

    video 39- Attaching a controller to the register from

--- controller/creative-db.php
+++ controller/creative-db.php
@@ -24,7 +24,7 @@
 			. "id int(11) NOT NULL AUTO_INCREMENT,"
 			. "username varchar(30) NOT NULL,"
 			. "email varchar(50) NOT NULL,"
-			. "password char(128 NOT NULL,"
+			. "password char(128) NOT NULL,"
 			. "salt char(128) NOT NULL,"
 			. "PRIMARY KEY (id))");
 		// if/else statement checks if we properly created a database 
--- view/register-form.php
+++ view/register-form.php
@@ -1,5 +1,14 @@
+<?php
+//helps us reach certian files with minimal work
+	require_once(__DIR__ . "/../model/config.php");
+?>
+
+<!-- Creates header for webpage -->
+<h1>Register</h1>
+<!-- method sends the info in form to our database;action creates the path to follow to get create user -->
+<!-- action creates a path to create-users -->
 <!-- Form created so user can register so they can be able to post, posts -->
-<form>
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
 <!-- label for user to register their email -->
 	<div>
 		<label for="email">Email: </label>
