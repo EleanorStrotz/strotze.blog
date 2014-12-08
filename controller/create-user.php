33f98683ec0671475b80f7d37135b66eab19f003
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-08 12:04:22 -0800

    video 42- storing the register form information

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,3 +1,14 @@
 <!--  -->
 <?php
-	echo "Successfully created user";
+// require once links config.php to create-user.php
+	require_once(__DIR__ . "/../model/config.php");
+
+	//inputs email to database
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	//inputs username to database
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	//inputs password to database
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	// echos out email, username, password
+	echo $email . " - " . $username . " - " . $password;
