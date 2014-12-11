55055efda6e1456db4d3c8ef88b4b81b11e3b4da
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-10 21:13:43 -0800

    video 51- protecting controllers and views

--- view/form.php
+++ view/form.php
@@ -1,6 +1,16 @@
 <?php
-//helps us reach certian files with minimal work
+	//links form.php to config.php
 	require_once(__DIR__ . "/../model/config.php");
+	// links form.php to login-verify.php
+	require_once(__DIR__ . "/../controller/login-verify.php");
+
+	// if statement runs authenticateUser
+	// within the if statement we run certain information
+	// die will stop the page from loading
+	if(!authenticateUser()) {
+		header("Location; " . $path . "index.php");
+		die();
+	}
 ?>
 
 <h1>Create Blog Post</h1>
