26f0f4da233e91cadbaa784e36c0a472bda614e7
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-10 20:27:11 -0800

    video 48- checking the passwords for login

--- controller/login-user.php
+++ controller/login-user.php
@@ -2,7 +2,7 @@
 	// require once links config.php file to login-user.php file
    require_once(__DIR__ . "/..model/config.php");	
 
-   //
+   // username and password variable help you login with authenication
    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
@@ -12,6 +12,19 @@
 	//checks if information is stored in the query variable
    if($query->num_rows == 1) {
    	 $row = $query->fetch_array();
+
+   	 //checks if the login was successful
+   	 if($row["password"] == crypt($password, $row["salt"])) {
+   	 	echo "<p>Login Successful</p>";
+   	 }
+   	 // else statement echos if the if statemt was incorrect
+   	 else {
+   	 	echo "<p>Invaild username and password</p>";
+   	 }
+   }
+   // else statement echos if the if statment for $query is not working
+   else {
+   	echo "<p>Invalid username and password</p>";
    }
 
 ?>
