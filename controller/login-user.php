304f8582d6ce78294de1190dabb21ad565a37dce
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-10 20:05:51 -0800

    video 47- selecting user information from the database

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,5 +1,17 @@
 <?php
-   // echo works if the code in login-forms code is correct
-	echo "Successfully logged in user";
+	// require once links config.php file to login-user.php file
+   require_once(__DIR__ . "/..model/config.php");	
+
+   //
+   $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+   $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+   //query selects salt password from a username in our users table.
+   $query = $_SESSION["connection"]->query("SELECT  salt,password FROM users WHERE username = '$username'");
+	
+	//checks if information is stored in the query variable
+   if($query->num_rows == 1) {
+   	 $row = $query->fetch_array();
+   }
 
 ?>
