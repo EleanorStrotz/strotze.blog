54b80936ac03e0496f1c4bc6095c99f0f1d5eb08
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-16 11:23:25 -0800

    currently fixing errors

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,9 @@
 <!-- script tag is the jquery in the code -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<!--  script tag links footer.php with bootstap.min.js-->
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <!-- script tag links footer.php with readmore.min.js -->
 <script type="text/javascript" src="js/readmore.min.js"></script>
+<!--  script tag links footer.php with bootstap.min.js-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 <!--  script tag links footer.php with main.js-->
 <script type="text/javascript" src="js/main.js"></script>
 
